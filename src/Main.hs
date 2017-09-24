7c3d3f5dc037c6479f126df9e852b0e20effb3f0
Mike Hewitt <matroxmike@icloud.com>
2017-09-24 11:05:12 +1000

    after hindent and shell scripts

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
 module Main where
-import Lib2
-import Lib1
-import Lib0
+
 import Data.Hex
+import Lib0
+import Lib1
+import Lib2
 
 main :: IO ()
 main = do
-    someFunc
-    someFunc1
-    someFunc2
-    putStrLn $ "d"
-    putStrLn $ hex "d"
-    putStrLn $ hex "9"
+  someFunc
+  someFunc1
+  someFunc2
+  putStrLn $ "d"
+  putStrLn $ hex "d"
+  putStrLn $ hex "9"
 -- added a call to an external routine 
 -- .cabal file just needed to mention hex
-   
